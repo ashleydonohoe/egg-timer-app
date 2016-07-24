c92658256b5901d43ff61fb6c9abe74f60b8af2a
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-07-24 00:02:47 -0400

    working on timer

--- Menu Bars/ViewController.swift
+++ Menu Bars/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    var timer = Timer()
+    
+    var seconds = 210
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        timerLabel.text = String(seconds)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonPressed(_ sender: AnyObject) {
+        timer.invalidate()
+        print("Timer Stopped")
+    }
+    
+    func processTimer() {
+        seconds += 1
+        timerLabel.text = String(seconds)
+    }
+    
+    
+    @IBAction func removeTen(_ sender: AnyObject) {
+        
+    }
+    
+    @IBAction func addTen(_ sender: AnyObject) {
+        
+    }
+    
+    @IBAction func resetTimer(_ sender: AnyObject) {
+        timer.invalidate()
+        seconds = 210
+        timerLabel.text = String(seconds)
+    }
+    
+    @IBAction func startTimer(_ sender: AnyObject) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processTimer), userInfo: nil, repeats: true)
 
+    }
+    
+    @IBAction func pauseTimer(_ sender: AnyObject) {
+        
+    }
 }
 
