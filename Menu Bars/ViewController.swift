88dc168547aed6371ed84bb1cf37a497d8ecee8f
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-07-23 23:15:29 -0400

    Initial Commit

---
+++ Menu Bars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Menu Bars
+//
+//  Created by Gabriele on 7/23/16.
+//  Copyright © 2016 Ashley Donohoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
